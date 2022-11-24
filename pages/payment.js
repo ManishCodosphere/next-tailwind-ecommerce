8e3fabb8ad61b1af3d13fe4db796e91462a2a277
manishgowda5563 <manish.gowda@wyzebulb.com>
2022-11-24 18:57:50 +0530

    feat: :art: Order summay

--- pages/payment.js
+++ pages/payment.js
@@ -68,3 +68,6 @@ export default function PaymentScreen() {
     </Layout>
   )
 }
+
+PaymentScreen.auth  = true;
+
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,73 +1,86 @@
-import Image from 'next/image';
-import Link from 'next/link'
-import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
-import Layout from '../../components/Layout';
-import data from '../../utils/data';
-import {Store} from '../../utils/Store'
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useContext } from "react";
+import { toast } from "react-toastify";
+import Layout from "../../components/Layout";
+import Product from "../../models/Product";
+import db from "../../utils/db";
+import { Store } from "../../utils/Store";
 
-export default function ProductScreen() {
-    const {state,dispatch} = useContext(Store);
-    const  router = useRouter();
-    const {query} = useRouter();
-    const {slug} =query;
-    const product =data.products.find((x) => x.slug === slug);
-    if (!product){
-        return<div>product not found</div>;
+export default function ProductScreen(props) {
+    const { product } = props;
+    const { state, dispatch } = useContext(Store);
+    const router = useRouter();
+    if (!product) {
+        return <Layout title="Product Not Found">product not found</Layout>;
     }
 
-const addToCartHandler  = () => {
-    const existItem = state.cart.cartItems.find((x) => x.slug === slug);
-    const quantity = existItem? existItem.quantity+1:1;
+    const addToCartHandler = async () => {
+        const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
 
-    if(product.countInStock < quantity){
-        alert('Sorry. Product is out of stock');
-        return;
-    }
-    dispatch({type:"CART_ADD_ITEM",payload:{...product, quantity}});
-    router.push('/cart');
-};
+        if (data.countInStock < quantity) {
+            return toast.error("Sorry. Product is out of stock");
+        }
+        dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+        router.push("/cart");
+    };
 
-  return (
-   <Layout title={product.name}>
-      <div className="py-2">
-          <Link href="/"> back to products</Link>
-      </div>
-      <div className='grid md:grid-cols-4 md:gap-3'>
-          <div className="md:col-span-2">
-              <Image
-              src={product.image}
-              alt={product.name}
-              width={640}
-              height={640}
-              layout="responsive">
-              </Image>
+    return (
+        <Layout title={product.name}>
+            <div className="py-2">
+                <Link href="/"> back to products</Link>
             </div>
-            <div>
-                <ul>
-                    <li>
-                    <h1 className="text-lg">{product.name}</h1>
-                    </li>
-                    <li>category: {product.category}</li>
-                    <li>Brand: {product.brand}</li>
-                    <li>{product.rating} of {product.numReviews} reviews</li>
-                    <li>Discription: {product.description}</li>
-                </ul>
+            <div className="grid md:grid-cols-4 md:gap-3">
+                <div className="md:col-span-2">
+                    <Image src={product.image} alt={product.name} width={640} height={640} layout="responsive"></Image>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h1 className="text-lg">{product.name}</h1>
+                        </li>
+                        <li>category: {product.category}</li>
+                        <li>Brand: {product.brand}</li>
+                        <li>
+                            {product.rating} of {product.numReviews} reviews
+                        </li>
+                        <li>Discription: {product.description}</li>
+                    </ul>
+                </div>
+                <div>
+                    <div className="card p-5">
+                        <div className="mb-2 flex justify-between">
+                            <div>Price</div>
+                            <div>Rs{product.price} </div>
+                        </div>
+                        <div className="mb-2 flex justify-between">
+                            <div>Status</div>
+                            <div>{product.countInStock > 0 ? "In Stock" : "Unavailable"} </div>
+                        </div>
+                        <button className="primary-button w-full" onClick={addToCartHandler}>
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div className='card p-5'>
-                <div className='mb-2 flex justify-between'>
-                    <div>Price</div>
-                    <div>Rs{product.price}  </div>
-                </div>
-                <div className='mb-2 flex justify-between'>
-                    <div>Status</div>
-                    <div>{product.countInStock > 0 ? 'In Stock':'Unavailable'} </div>
-                </div>
-                <button className='primary-button w-full' onClick={addToCartHandler}>Add to cart</button>
-                </div>
-            </div>
-      </div>
-   </Layout>
-  );
+        </Layout>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    await db.connect();
+    const product = await Product.find({ slug }).lean();
+    await db.disconnect();
+    return {
+        props: {
+            product: product ? db.convertDocToObj(product[0]) : null,
+        },
+    };
 }
